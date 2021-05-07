4a739e47ee9f2ac4ab642ded619758246c63b234
Roy Corella <corellasanchez@gmail.com>
2021-05-06 18:42:14 -0600

    idle

---
+++ personaje.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+onready var animation = get_node("AnimationPlayer")
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_process_input(true)
+
+	
+func _fixed_process(delta):
+	var State = "Descanso"
+	print(animation.get_current_animation())	
+	if animation.get_current_animation() == State:
+		print("Hello")
+	#  animation.play(State) 
+
+
